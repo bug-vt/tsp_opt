ad26e15c438239f424ccaee132480360454cdaf0
Bug Lee <bug19@vt.edu>
2023-05-03 14:40:10 -0400

    Added City class and all function to compute all possible permutation of cities.

--- tsp.py
+++ tsp.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python3
 
+import random
+
+class City:
+  def __init__ (self, name):
+    self.name = name
+    self.x = random.randint (0, 500)
+    self.y = random.randint (0, 500)
+
+  def __str__ (self):
+    return str (self.name)
+
+  def __repr__ (self):
+    return str (self)
+
+
 n_max = 4
-cities = [x for x in range (n_max)]
+cities = [City(i) for i in range (n_max)]
 visited = [None] * n_max
+
 
 def dist (a, b):
   return sqrt ( (a.x - b.x)**2 + (a.y - b.y)**2 )
