c71f4d92f18d6ef4f2d558f0350ae80a253bcc30
Bug Lee <bug19@vt.edu>
2023-05-03 14:58:50 -0400

    Added brute force tsp to try all permutation and find the best route.

--- tsp.py
+++ tsp.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import random
+import math
 
 class City:
   def __init__ (self, name):
@@ -18,17 +19,31 @@ class City:
 n_max = 4
 cities = [City(i) for i in range (n_max)]
 visited = [None] * n_max
+tsp_route = [None] * n_max
+min_total_dist = math.inf
 
 
 def dist (a, b):
-  return sqrt ( (a.x - b.x)**2 + (a.y - b.y)**2 )
+  return math.sqrt ( (a.x - b.x)**2 + (a.y - b.y)**2 )
 
+def calc_total_dist (visited):
+  global min_total_dist
+  global tsp_route
+
+  total_dist = 0
+  for i in range (len (visited) - 1):
+    total_dist += dist (visited[i], visited[i+1]) 
+
+  if total_dist < min_total_dist:
+    min_total_dist = total_dist
+    tsp_route = [visited[i] for i in range (len (visited))]
 
 def tsp_unopt (cities):
   # base case
   if len (cities) == 0:
-    print (visited)
+    calc_total_dist (visited)
     return 0
+
   for i in range (len (cities)):
     visited[len (cities) - 1] = cities[i] 
     tsp_unopt (cities[:i] + cities[i + 1:])
@@ -36,7 +51,8 @@ def tsp_unopt (cities):
 
 def main ():
   tsp_unopt (cities)
-
+  print (min_total_dist)
+  print (tsp_route)
 
 if __name__ == "__main__":
   main ()
